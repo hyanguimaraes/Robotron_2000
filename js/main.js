8571e395d9f380906a64972280fcd9ade8a6777e
hyanguimaraes <hyangt@hotmail.com>
2023-06-08 10:03:25 -0300

    Commit final Aula 03
    Commit com os arquivos contendo todas as alterações até o final da Aula 03

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-const robotron = document.querySelector("#Robotron");
+//Constantes dos botões de somar ou subtrair braços;
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento);
-});
+//Constante que captura todos os controles de ajuste em um array
+const controle = document.querySelectorAll(".controle-ajuste");
+console.log(controle)
 
-function dizOi(nome){
-    console.log(Oi + nome);
-    console.log("Bem vindo ao Robotron 2000");
+//Utiliza o array "controle" para capturar o texto do controle que foi clicado (se for um botão de soma, captura "+" e vice-versa)
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento)=>{
+        manipulaDados(evento.target.textContent);
+    })
+})
+
+//Função que manipula os dados dos braços
+function manipulaDados(operacao){
+    if(operacao === "-"){
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
 }
-
-dizOi("Pedro");
